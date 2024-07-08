88d71c99bcf708e492dc495fbb441eefacae04e4
Nhi Nguyen <thaonhi8a8@gmail.com>
2024-07-08 23:42:59 +0700

    Resolve almostIncreasingSequence

--- 100AlgorithmsChallenge/almostIncreasingSequence/almostIncreasingSequence.ts
+++ 100AlgorithmsChallenge/almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,5 +1,14 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
+  let temp = 0;
+  const length = sequence.length;
 
+  sequence.forEach((item, index) => {
+    if (item > sequence[index+1] && index > length - 1) {
+        temp = temp + 1
+    }
+  })
+
+  return temp <= 1;
 }
 
 // console.log(almostIncreasingSequence([1, 3, 2, 1])) 
